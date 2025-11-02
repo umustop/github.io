75a15d682bde95c11bd124d3eb1b65e3d76c67a8
umustop <9767077@gmail.com>
2025-11-02 21:41:23 +0800

    Site updated: 2025-11-02 21:41:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/02/hello-world/"];function toRandomPost(){
+var posts=["2025/07/21/LoveStory/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
