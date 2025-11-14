ca77a0cc8b6a5e7dd63434a888cd664ffa85f0a0
umustop <9767077@gmail.com>
2025-11-14 14:28:35 +0800

    Site updated: 2025-11-14 14:28:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/21/LoveStory/"];function toRandomPost(){
+var posts=["2024/11/05/電視連《走向共和》｜孫中山先生演講全文/","2025/07/21/LoveStory/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
